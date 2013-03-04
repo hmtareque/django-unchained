b26542bf50986f8b5274f6627f82c7449f660e60
Hasan Tareque <hmtareque@gmail.com>
2013-03-04 14:04:09 +0000

    admin settings updated

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Task
 
+admin.site.site_header = 'Todo App'
+admin.site.site_title = 'Todo App Admin'
+admin.site.index_title = 'Welcome to Todo App'
+
 admin.site.register(Task)
