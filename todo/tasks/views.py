030d68f7f2efd5263c00ab0e6e0642e0cec62ede
Hasan Tareque <hmtareque@gmail.com>
2013-03-04 14:17:59 +0000

    mvc in action

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Task
+
+# Get tasks and display 
+def index(request):
+    return render(request, 'tasks/index.html')
