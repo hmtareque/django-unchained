b64a0c5a015702796cdb4b4545bbf8ffc9d63824
Hasan Tareque <hmtareque@gmail.com>
2013-03-05 16:24:21 +0000

    task list view added

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'tasks'
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('create', views.create, name="create"),
+    path('/<int:task_id>/complete', views.complete, name="complete"),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -9,3 +9,11 @@ def index(request):
     tasks = Task.objects.all()
     context = {'tasks': tasks}
     return render(request, 'tasks/index.html', context)
+
+# Create new task
+def create(request):
+    return render(request, 'tasks/create.html')
+
+# Complete a task
+def complete(request, task_id):
+    return render(request, 'tasks/create.html')
